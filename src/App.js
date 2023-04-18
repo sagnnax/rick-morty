cd59edf8e9802995f8bc665c3fed4bdd3237210d
sagnnax <axel.gustavo2001@outlook.com>
2023-04-17 23:58:47 -0600

    add code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
+import credencial from './img/foto.jpg';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  };
+
+  console.log(characters)
+
   return (
     <div className="App">
+      <h1 className="title">Rick & Morty API</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={credencial} alt="center" width="190" align="top"></img>
+        <div style={{ color: "#89D325", fontSize: "1em", fontFamily: "Times" }}>By sag</div>
+        <div style={{ color: "rgb(62, 199, 199)", fontSize: "1em", fontFamily: "Times" }}>S20006742 - Axel Gustavo Peña Sánchez</div>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
+          </>
+        )}
       </header>
     </div>
   );
